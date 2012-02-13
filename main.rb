f93e45f8b42eacebba5a121e7f252608a01c1367
Max Gonzih <gonzih@gmail.com>
2012-02-13 18:08:44 +0300

    render world without blinking

--- main.rb
+++ main.rb
@@ -10,8 +10,9 @@ width  = `tput cols`.to_i  || 40
 world = World.new(width, height)
 
 loop do
+  output = world.to_s
   system('clear')
-  puts world
+  puts output
   world.next!
   sleep(0.1)
 end
