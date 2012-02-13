d0b34ac4eaf1d4128558a90fa292c444e6f7ecf6
Max Gonzih <gonzih@gmail.com>
2012-02-13 17:03:07 +0300

    add sample run in main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,14 @@
 require 'bundler'
 Bundler.require(:default) if defined?(Bundler)
+
+require './lib/world'
+require './lib/cell'
+
+world = World.new(4, 3)
+
+10.times do
+  puts world
+  world.next!
+  sleep(2)
+  system('clear')
+end
