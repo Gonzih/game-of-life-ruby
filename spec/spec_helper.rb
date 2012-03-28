2f5627f3a59f3a50bbadceec78ac22c074d918aa
Max Gonzih <gonzih@gmail.com>
2012-03-28 10:51:11 +0300

    add filter to simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'bundler'
 require 'simplecov'
+SimpleCov.configure do
+  add_filter do |file|
+    file.filename !~ /lib\/game_of_life.*\.rb/
+  end
+end
 SimpleCov.start
 require 'game_of_life'
 require 'game_of_life/renderers/cli'
